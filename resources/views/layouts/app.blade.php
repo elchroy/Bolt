e2bb6d39c7db950d7465a9a1cec2673fdcf5995e
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-05-11 02:00:42 +0100

    Add style and css to the body element

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/cage.css') }}">
 
     <style>
         body {
@@ -25,7 +26,7 @@
         }
     </style>
 </head>
-<body id="app-layout">
+<body id="bolt-layout">
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
